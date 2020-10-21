addda1619f32d58613cdc94121e21eb4798418b9
hyilmi <hetgin@gmail.com>
2020-10-21 13:04:43 +0200

    Eigen oplossing

--- oplossing/code.js
+++ oplossing/code.js
@@ -19,6 +19,32 @@ function generate_puzzle_html(puzzle){
     //TODO: Implementeer deze functie!
     //puzzle bevat een tweedimensionale lijst die de sliding puzzle voorstelt
     //Kijk naar de functie generate_board_html in voorbeeld 7 uit het hoorcollege voor inspiratie
+    
+    let table_inner_html = "";
+    for(let i = 0; i < puzzle.length; i++){
+        let row_html = "<tr>"
+        
+        for(let j = 0; j < puzzle[i].length; j++){
+            row_html += generate_square(puzzle[i][j]);
+        }
+        row_html += "</tr>";
+        table_inner_html += row_html;
+    }
+
+    return `<table>${table_inner_html}</table>`;
+}
+
+function generate_square(number){
+    let square_class = "empty";
+    let square_content = number;
+
+    if(number == 0){
+        return `<td class="empty">0</td>`;
+    }else {
+        return `<td>${square_content}</td>`;
+    }
+
+
 }
 
 
